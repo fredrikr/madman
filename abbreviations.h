46a26f927cc60897d95b071bfdd0b7d2ed692a82
framsberg <fredrik.ramsberg@gmail.com>
2026-07-19 20:40:55 +0200

    Updated to Release 1, Serial 260719

--- abbreviations.h
+++ abbreviations.h
@@ -2,95 +2,95 @@ Abbreviate "Oops! It feels very odd inside. I'm now";!     3x45, saved    84
 Abbreviate "The knife made no impression on it.";!     2x37, saved    31
 Abbreviate ", I discover a hole in the floor.^";!     2x38, saved    33
 Abbreviate "I'd cut myself, for sure.";     !     2x29, saved    24
+Abbreviate "Do you think I can w";          !     3x22, saved    36
+Abbreviate "[Command recording o";          !     2x24, saved    20
 Abbreviate "find nothing special";          !     3x20, saved    33
 Abbreviate "tands slightly ajar.";          !     2x21, saved    17
-Abbreviate "[Command recording o";          !     2x24, saved    20
-Abbreviate "ink I can walk throu";          !     3x21, saved    36
 Abbreviate "AROUND!!^HE SHOT ME";           !     3x36, saved    66
 Abbreviate "llet-proof jacket";             !     3x18, saved    30
 Abbreviate "AOW! That hurts.";              !     2x22, saved    16
-Abbreviate " has now killed";               !     2x15, saved    11
-Abbreviate "I don't have a";                !     4x16, saved    38
 Abbreviate "Nothing happen";                !     3x15, saved    24
 Abbreviate "Pill. Colour: ";                !     2x18, saved    14
-Abbreviate "on't want to ";                 !     5x14, saved    45
 Abbreviate "Use WHAT to s";                 !     2x18, saved    14
-Abbreviate "ubway statio";                  !     4x12, saved    28
-Abbreviate "providing l";                   !     3x11, saved    15
+Abbreviate "on't want to ";                 !     5x14, saved    45
+Abbreviate "The madman ";                   !     8x12, saved    68
+Abbreviate "bway statio";                   !     4x11, saved    24
 Abbreviate " FEET TALL";                    !     2x18, saved    14
-Abbreviate "something";                     !     5x 9, saved    26
 Abbreviate "I'm in a ";                     !    23x11, saved   195
-Abbreviate "ranscript";                     !     6x 9, saved    33
+Abbreviate "something";                     !     5x 9, saved    26
 Abbreviate "BOOOOOM!!";                     !     4x18, saved    46
-Abbreviate "corridor";                      !     3x 8, saved     9
-Abbreviate "o heavy.";                      !     3x 9, saved    12
-Abbreviate " VANISHE";                      !     4x15, saved    37
-Abbreviate "arehouse";                      !     9x 8, saved    45
+Abbreviate "ranscript";                     !     6x 9, saved    33
+Abbreviate " of glass";                     !     4x 9, saved    19
+Abbreviate "arehouse";                      !    10x 8, saved    51
 Abbreviate " persons";                      !     4x 8, saved    15
-Abbreviate " button";                       !     7x 7, saved    26
-Abbreviate "lready ";                       !    13x 7, saved    56
-Abbreviate "Closed ";                       !     6x 8, saved    27
-Abbreviate " turns ";                       !     4x 7, saved    11
-Abbreviate " WHAT??";                       !    21x13, saved   216
-Abbreviate "Inform ";                       !     3x 8, saved     9
+Abbreviate " VANISHE";                      !     4x15, saved    37
 Abbreviate "ardrobe";                       !     4x 7, saved    11
-Abbreviate "art of";                        !     6x 6, saved    18
-Abbreviate "ittle ";                        !     5x 6, saved    14
-Abbreviate " seems";                        !     5x 6, saved    14
-Abbreviate "arbour";                        !     6x 6, saved    18
-Abbreviate " from ";                        !     6x 6, saved    18
-Abbreviate "utside";                        !     4x 6, saved    10
-Abbreviate "Can't ";                        !     7x 8, saved    33
-Abbreviate "madman";                        !    11x 6, saved    38
+Abbreviate "lready ";                       !    13x 7, saved    56
+Abbreviate " button";                       !     7x 7, saved    26
+Abbreviate "o heavy";                       !     4x 7, saved    11
+Abbreviate " WHAT??";                       !    21x13, saved   216
+Abbreviate "I can't";                       !    10x 9, saved    61
+Abbreviate "Closed ";                       !     6x 8, saved    27
 Abbreviate "CLICK!";                        !     4x12, saved    28
-Abbreviate "have ";                         !     9x 5, saved    21
-Abbreviate " and ";                         !     9x 5, saved    21
+Abbreviate " seems";                        !     6x 6, saved    18
+Abbreviate "arbour";                        !     6x 6, saved    18
+Abbreviate "ticket";                        !     6x 6, saved    18
+Abbreviate "Can't ";                        !     7x 8, saved    33
+Abbreviate "ittle ";                        !     6x 6, saved    18
+Abbreviate "art of";                        !     6x 6, saved    18
+Abbreviate "ed to ";                        !    10x 6, saved    34
+Abbreviate "I can ";                        !     5x 7, saved    16
+Abbreviate "It's ";                         !    28x 7, saved   131
+Abbreviate " from";                         !     6x 5, saved    12
 Abbreviate "room.";                         !     9x 6, saved    30
-Abbreviate " the ";                         !    40x 5, saved   114
-Abbreviate " that";                         !    11x 5, saved    27
-Abbreviate "NAIL ";                         !     3x 9, saved    12
-Abbreviate " with";                         !     7x 5, saved    15
-Abbreviate "It's ";                         !    26x 7, saved   121
-Abbreviate " need";                         !     7x 5, saved    15
-Abbreviate "AAAH!";                         !     4x10, saved    20
 Abbreviate "close";                         !     6x 5, saved    12
-Abbreviate "n't ";                          !    20x 5, saved    54
-Abbreviate "here";                          !    19x 4, saved    32
-Abbreviate "The ";                          !    31x 5, saved    87
-Abbreviate " of ";                          !    17x 4, saved    28
-Abbreviate "ing ";                          !    28x 4, saved    50
-Abbreviate "That";                          !    15x 5, saved    39
-Abbreviate " to ";                          !    27x 4, saved    48
-Abbreviate " you";                          !    14x 4, saved    22
-Abbreviate "Some";                          !    11x 5, saved    27
-Abbreviate "lock";                          !    11x 4, saved    16
-Abbreviate " it.";                          !    10x 5, saved    24
-Abbreviate "Rope";                          !     5x 5, saved     9
-Abbreviate "Open";                          !     5x 5, saved     9
-Abbreviate " key";                          !     7x 4, saved     8
-Abbreviate "You ";                          !     7x 5, saved    15
-Abbreviate "door";                          !    11x 4, saved    16
+Abbreviate " with";                         !     8x 5, saved    18
+Abbreviate "have ";                         !    14x 5, saved    36
+Abbreviate " that";                         !    13x 5, saved    33
+Abbreviate " the ";                         !    46x 5, saved   132
+Abbreviate " and ";                         !    13x 5, saved    33
+Abbreviate "AAAH!";                         !     4x10, saved    20
+Abbreviate "roof.";                         !     4x 6, saved    10
+Abbreviate " of ";                          !    14x 4, saved    22
+Abbreviate "The ";                          !    26x 5, saved    72
+Abbreviate "n't ";                          !    21x 5, saved    57
+Abbreviate "ing ";                          !    33x 4, saved    60
+Abbreviate " to ";                          !    19x 4, saved    32
+Abbreviate "here";                          !    21x 4, saved    36
+Abbreviate "That";                          !    16x 5, saved    42
 Abbreviate "I'm ";                          !    32x 6, saved   122
-Abbreviate " is ";                          !    15x 4, saved    24
-Abbreviate "I ca";                          !    13x 5, saved    33
-Abbreviate " in ";                          !    12x 4, saved    18
-Abbreviate "'s ";                           !    11x 4, saved    16
-Abbreviate " th";                           !    16x 3, saved    13
-Abbreviate " to";                           !    13x 3, saved    10
-Abbreviate " a ";                           !    21x 3, saved    18
-Abbreviate "ed.";                           !     9x 4, saved    12
+Abbreviate "Some";                          !    11x 5, saved    27
+Abbreviate "door";                          !    12x 4, saved    18
+Abbreviate "off.";                          !     6x 5, saved    12
+Abbreviate "now ";                          !     7x 4, saved     8
+Abbreviate " any";                          !     7x 4, saved     8
+Abbreviate " you";                          !    13x 4, saved    20
+Abbreviate "You ";                          !     7x 5, saved    15
+Abbreviate " it.";                          !    10x 5, saved    24
+Abbreviate "ight";                          !     9x 4, saved    12
+Abbreviate "lock";                          !    11x 4, saved    16
+Abbreviate " in ";                          !    15x 4, saved    24
+Abbreviate " is ";                          !    16x 4, saved    26
+Abbreviate " a ";                           !    26x 3, saved    23
+Abbreviate "ing";                           !    24x 3, saved    21
+Abbreviate " st";                           !    14x 3, saved    11
+Abbreviate "all";                           !    14x 3, saved    11
+Abbreviate "for";                           !    10x 3, saved     7
+Abbreviate "ear";                           !    12x 3, saved     9
+Abbreviate "'s ";                           !    13x 4, saved    20
+Abbreviate "...";                           !    16x 6, saved    58
 Abbreviate "OK^";                           !    16x 6, saved    58
-Abbreviate "...";                           !    17x 6, saved    62
-Abbreviate "ot ";                           !    17x 3, saved    14
-Abbreviate "ing";                           !    23x 3, saved    20
-Abbreviate "I ";                            !    30x 3, saved    27
-Abbreviate ", ";                            !    21x 3, saved    18
-Abbreviate "e.";                            !    17x 3, saved    14
+Abbreviate " th";                           !    17x 3, saved    14
+Abbreviate "one";                           !    18x 3, saved    15
+Abbreviate "ed.";                           !    10x 4, saved    14
+Abbreviate "ot ";                           !    18x 3, saved    15
+Abbreviate "I ";                            !    32x 3, saved    29
+Abbreviate "!!";                            !    13x 4, saved    20
+Abbreviate "OK";                            !    12x 4, saved    18
+Abbreviate "!^";                            !    22x 4, saved    38
 Abbreviate ".^";                            !    22x 4, saved    38
 Abbreviate "St";                            !    15x 3, saved    12
-Abbreviate "!^";                            !    20x 4, saved    34
-Abbreviate "OK";                            !    11x 4, saved    16
+Abbreviate "I'";                            !     8x 4, saved    10
+Abbreviate ". ";                            !    29x 3, saved    26
+Abbreviate ", ";                            !    24x 3, saved    21
 Abbreviate ".]";                            !     4x 6, saved    10
-Abbreviate "No";                            !     7x 3, saved     4
-Abbreviate ". ";                            !    23x 3, saved    20
-Abbreviate "!!";                            !    13x 4, saved    20
